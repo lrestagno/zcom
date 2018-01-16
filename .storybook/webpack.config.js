3a30d5e1697a9a565bca3f05a4f90fd4233584c9
mzapaia <mzapaia@MACMZAPAIA.local>
2018-01-16 12:36:59 -0300

    fix r.loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,7 +3,7 @@ const babelOptions = require('../babel-options');
 
 module.exports = (storybookBaseConfig, configType) => {
   storybookBaseConfig.module.rules.forEach(r=>{
-    if(r.loader.includes('babel')){
+    if(r.loader && r.loader.includes('babel')){
       r.test = /(\.jsx?$|\.svg$)/;
       r.include.push(process.env.STORYBOOK_CURRENT_DIR)
       r.exclude.push(process.env.STORYBOOK_CURRENT_DIR+'/node_modules')
